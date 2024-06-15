863420c8da4c593a5af26268f4b1fa0a346c6e38
QorbQuant <qorbanferrell@gmail.com>
2024-06-15 08:47:40 -0700

    Add front page

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def front_page():
+    return render_template('front_page.html')
+
+@app.route('/dashboard')
+def dashboard():
     df = pd.read_csv('query_result.csv')
     data = df.to_dict(orient='records')
     return render_template('index.html', data=data)
